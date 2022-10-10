b45b00bb4f7bf6aee634083520d05fbb2e81afb1
Allen Pocket <AllenPocketGamer@Gmail.com>
2022-10-10 21:05:20 +0800

    :tada: ($PROJ) Init project

---
+++ src/ICAgentFFI/Principal.cs
@@ -0,0 +1,39 @@
+using System.Runtime.InteropServices;
+
+using StateCode = System.Int32;
+
+namespace IC
+{
+    public struct Principal
+    {
+        public static void Test()
+        {
+            byte[] out_arr = new byte[64];
+            UInt32 out_arr_len = 0;
+
+            {
+                var sc = FFIMethods.principal_management_canister(out_arr, out out_arr_len, (UInt32)out_arr.Length);
+                Console.WriteLine($"{sc}: {string.Join(",", out_arr)}, {out_arr_len}");
+            }
+
+            {
+                var sc = FFIMethods.principal_anonymous(out_arr, out out_arr_len, (UInt32)out_arr.Length);
+                Console.WriteLine($"{sc}: {string.Join(",", out_arr)}, {out_arr_len}");
+            }
+        }
+    }
+
+    internal static class FFIMethods
+    {
+        internal const StateCode SC_OK = 0;
+        internal const StateCode SC_DATA_OVERFLOW = -1;
+        internal const StateCode SC_INTERNAL_ERR = -2;
+        internal const StateCode SC_ERR_INFO_OVERFLOW = -3;
+
+        [DllImport("/Users/allen/agent-unity-laboratory/src/ICAgentFFI/ic-agent-ffi.dylib", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern StateCode principal_management_canister(byte[] out_arr, out UInt32 out_arr_len, in UInt32 arr_len);
+
+        [DllImport("/Users/allen/agent-unity-laboratory/src/ICAgentFFI/ic-agent-ffi.dylib", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern StateCode principal_anonymous(byte[] out_arr, out UInt32 out_arr_len, in UInt32 arr_len);
+    }
+}
---
+++ tests/ICAgentFFI.Tests/Principal_ShouldWork.cs
@@ -0,0 +1,13 @@
+using IC;
+
+namespace ICAgentFFI.Tests
+{
+    public class Principal_ShouldWork
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            Principal.Test();
+        }
+    }
+}
