95a3c46fa70adb97ffd44b26afc6ba04bbe2705e
Allen Pocket <AllenPocketGamer@Gmail.com>
2022-10-12 10:06:51 +0800

    :sparkles: ($PRINCIPAL) complete 80% progress of Principal

--- src/ICAgentFFI/Principal.cs
+++ src/ICAgentFFI/Principal.cs
@@ -2,28 +2,186 @@ using System.Runtime.InteropServices;
 
 using StateCode = System.Int32;
 
-namespace IC
+namespace ICAgentFFI
 {
-    public struct Principal
+    public class Principal
     {
-        public static void Test()
-        {
-            byte[] out_arr = new byte[64];
-            UInt32 out_arr_len = 0;
+        private static byte[] OUT_ARR = new byte[32];
+        static uint OUT_ARR_LEN = 0;
+        static byte[] OUT_ERR_INFO = new byte[256];
 
+        static void ClearOutVars()
+        {
+            Array.Clear(OUT_ARR);
+            OUT_ARR_LEN = 0;
+
+            Array.Clear(OUT_ERR_INFO);
+        }
+
+        public byte[] Bytes { get; }
+
+        Principal()
+        {
+            var bytes = new byte[OUT_ARR_LEN];
+            Array.Copy(OUT_ARR, bytes, OUT_ARR_LEN);
+            this.Bytes = bytes;
+        }
+
+        public static Principal ManagementCanister()
+        {
+            ClearOutVars();
+
+            var sc = FromRust.principal_management_canister(OUT_ARR, out OUT_ARR_LEN, (UInt32)OUT_ARR.Length);
+
+            // If state code is out of range, make it fail.
+            if (sc != FromRust.SC_OK && sc != FromRust.SC_DATA_OVERFLOW) sc = Int32.MaxValue;
+
+            return Principal.PostProcess(sc);
+        }
+
+        public static Principal SelfAuthenticating(byte[] publicKey)
+        {
+            ClearOutVars();
+
+            var sc = FromRust.principal_self_authenticating(OUT_ARR, out OUT_ARR_LEN, (UInt32)OUT_ARR.Length, publicKey,
+                (UInt32)publicKey.Length);
+
+            // If state code is out of range, make it fail.
+            if (sc != FromRust.SC_OK && sc != FromRust.SC_DATA_OVERFLOW) sc = Int32.MaxValue;
+
+            return Principal.PostProcess(sc);
+        }
+
+        public static Principal Anonymous()
+        {
+            ClearOutVars();
+
+            var sc = FromRust.principal_anonymous(OUT_ARR, out OUT_ARR_LEN, (UInt32)OUT_ARR.Length);
+
+            // If state code is out of range, make it fail.
+            if (sc != FromRust.SC_OK && sc != FromRust.SC_DATA_OVERFLOW) sc = Int32.MaxValue;
+
+            return Principal.PostProcess(sc);
+        }
+
+        public static Principal FromBytes(byte[] bytes)
+        {
+            ClearOutVars();
+
+            var sc = FromRust.principal_from_bytes(
+                bytes,
+                (UInt32)bytes.Length,
+                OUT_ARR,
+                out OUT_ARR_LEN,
+                (UInt32)OUT_ARR.Length,
+                OUT_ERR_INFO,
+                (UInt32)OUT_ERR_INFO.Length
+            );
+
+            // If state code is out of range, make it fail.
+            if (
+                sc != FromRust.SC_OK &&
+                sc != FromRust.SC_DATA_OVERFLOW &&
+                sc != FromRust.SC_INTERNAL_ERR &&
+                sc != FromRust.SC_ERR_INFO_OVERFLOW
+            ) sc = Int32.MaxValue;
+
+            return Principal.PostProcess(sc);
+        }
+
+        public static Principal FromText(string text)
+        {
+            ClearOutVars();
+
+            var sc = FromRust.principal_from_text(
+                text,
+                OUT_ARR,
+                out OUT_ARR_LEN,
+                (UInt32)OUT_ARR.Length,
+                OUT_ERR_INFO,
+                (UInt32)OUT_ERR_INFO.Length
+            );
+
+            // If state code is out of range, make it fail.
+            if (
+                sc != FromRust.SC_OK &&
+                sc != FromRust.SC_DATA_OVERFLOW &&
+                sc != FromRust.SC_INTERNAL_ERR &&
+                sc != FromRust.SC_ERR_INFO_OVERFLOW
+            ) sc = Int32.MaxValue;
+
+            return Principal.PostProcess(sc);
+        }
+
+        /// Helper Function: Construct Result by StateCode and OUT Vars.
+        static Principal PostProcess(StateCode sc)
+        {
+            switch (sc)
             {
-                var sc = FFIMethods.principal_management_canister(out_arr, out out_arr_len, (UInt32)out_arr.Length);
-                Console.WriteLine($"{sc}: {string.Join(",", out_arr)}, {out_arr_len}");
+                case FromRust.SC_OK:
+                    return new Principal();
+                case FromRust.SC_DATA_OVERFLOW:
+                    throw new Exception($"Data Overflow: `OUT_ARR_LEN({OUT_ARR_LEN})` > `ARR_SIZE({OUT_ARR.Length})`.");
+                case FromRust.SC_INTERNAL_ERR:
+                    var len = Array.IndexOf(OUT_ERR_INFO, '\0');
+                    var errInfo = System.Text.Encoding.ASCII.GetString(OUT_ERR_INFO, 0, len);
+                    throw new Exception($"Internal: {errInfo}");
+                case FromRust.SC_ERR_INFO_OVERFLOW:
+                    throw new Exception(
+                        $"ErrInfo Overflow: The length of ErrInfo is bigger than `ERR_INFO_SIZE({OUT_ERR_INFO.Length})`.");
+                default:
+                    throw new Exception("Unknown: The StateCode returned is unexpected.");
             }
 
+            ;
+        }
+
+        public override string ToString()
+        {
+            ClearOutVars();
+
+            var OUT_TEXT = new byte[64];
+
+            var sc = FromRust.principal_to_text(
+                this.Bytes,
+                (UInt32)this.Bytes.Length,
+                OUT_TEXT,
+                (UInt32)OUT_TEXT.Length,
+                OUT_ERR_INFO,
+                (UInt32)OUT_ERR_INFO.Length
+            );
+
+            switch (sc)
             {
-                var sc = FFIMethods.principal_anonymous(out_arr, out out_arr_len, (UInt32)out_arr.Length);
-                Console.WriteLine($"{sc}: {string.Join(",", out_arr)}, {out_arr_len}");
+                case FromRust.SC_OK:
+                {
+                    var len = Array.IndexOf(OUT_TEXT, (byte)0);
+                    var principalText = System.Text.Encoding.ASCII.GetString(OUT_TEXT, 0, len);
+                    return principalText;
+                }
+                case FromRust.SC_DATA_OVERFLOW:
+                {
+                    throw new Exception(
+                        $"Data Overflow: The length of textual Principal is bigger than `OUT_TEXT({OUT_ERR_INFO.Length})`.`.");
+                }
+                case FromRust.SC_INTERNAL_ERR:
+                {
+                    var len = Array.IndexOf(OUT_ERR_INFO, '\0');
+                    var errInfo = System.Text.Encoding.ASCII.GetString(OUT_ERR_INFO, 0, len);
+                    throw new Exception($"Internal: {errInfo}");
+                }
+                case FromRust.SC_ERR_INFO_OVERFLOW:
+                {
+                    throw new Exception(
+                        $"ErrInfo Overflow: The length of ErrInfo is bigger than `ERR_INFO_SIZE({OUT_ERR_INFO.Length})`.");
+                }
+                default:
+                    throw new Exception("Unknown: The StateCode returned is unexpected.");
             }
         }
     }
 
-    internal static class FFIMethods
+    internal static class FromRust
     {
         internal const StateCode SC_OK = 0;
         internal const StateCode SC_DATA_OVERFLOW = -1;
@@ -31,9 +189,51 @@ namespace IC
         internal const StateCode SC_ERR_INFO_OVERFLOW = -3;
 
         [DllImport("ic-agent-ffi", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern StateCode principal_management_canister(byte[] out_arr, out UInt32 out_arr_len, in UInt32 arr_len);
+        internal static extern StateCode principal_management_canister(byte[] out_arr, out UInt32 out_arr_len,
+            UInt32 arr_len);
 
         [DllImport("ic-agent-ffi", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern StateCode principal_anonymous(byte[] out_arr, out UInt32 out_arr_len, in UInt32 arr_len);
+        internal static extern StateCode principal_self_authenticating(
+            byte[] out_arr,
+            out UInt32 out_arr_len,
+            UInt32 arr_size,
+            byte[] public_key,
+            UInt32 public_key_size
+        );
+
+        [DllImport("ic-agent-ffi",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern StateCode principal_anonymous(byte[] out_arr, out UInt32 out_arr_len, UInt32 arr_len);
+
+        [DllImport("ic-agent-ffi", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern StateCode principal_from_bytes(
+            byte[] bytes,
+            UInt32 bytes_size,
+            byte[] out_arr,
+            out UInt32 out_arr_len,
+            UInt32 arr_size,
+            byte[] out_err_info,
+            UInt32 err_info_size
+        );
+
+        [DllImport("ic-agent-ffi", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern StateCode principal_from_text(
+            string text,
+            byte[] out_arr,
+            out UInt32 out_arr_len,
+            UInt32 arr_size,
+            byte[] out_err_info,
+            UInt32 err_info_size
+        );
+
+        [DllImport("ic-agent-ffi", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern StateCode principal_to_text(
+            byte[] bytes,
+            UInt32 bytes_size,
+            byte[] out_text,
+            UInt32 text_size,
+            byte[] out_err_info,
+            UInt32 err_info_size
+        );
     }
 }
--- tests/ICAgentFFI.Tests/Principal_ShouldWork.cs
+++ tests/ICAgentFFI.Tests/Principal_ShouldWork.cs
@@ -1,13 +1,76 @@
-using IC;
-
 namespace ICAgentFFI.Tests
 {
-    public class Principal_ShouldWork
+    using ICAgentFFI;
+    
+    public class PrincipalTest
     {
         [Fact]
-        public void ShouldWork()
+        public void ManagementCanister_ShouldWork()
         {
-            Principal.Test();
+            var BYTES_EXPECTED = Array.Empty<byte>();
+
+            var principal = Principal.ManagementCanister();
+
+            Assert.Equal(principal.Bytes, BYTES_EXPECTED);
         }
+
+        [Fact]
+        public void SelfAuthenticating_ShouldWork()
+        {
+            byte[] PUBLIC_KEY = {
+                0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22,
+                0x11, 0x00, 0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44,
+                0x33, 0x22, 0x11, 0x00,
+            };
+
+            byte[] BYTES_EXPECTED = {
+                0x2f, 0x8e, 0x47, 0x38, 0xf9, 0xd7, 0x68, 0x16, 0x82, 0x99, 0x85, 0x41, 0x52, 0x67,
+                0x86, 0x38, 0x07, 0xd3, 0x7d, 0x20, 0x6a, 0xd9, 0x0f, 0xea, 0x72, 0xbf, 0x9d, 0xcf,
+                0x02,
+            };
+
+            var principal = Principal.SelfAuthenticating(PUBLIC_KEY);
+
+            Assert.Equal(principal.Bytes, BYTES_EXPECTED);
+        }
+
+        [Fact]
+        public void Anonymous_ShouldWork()
+        {
+            byte[] BYTES_EXPECTED = { 4 };
+
+            var principal = Principal.Anonymous();
+
+            Assert.Equal(principal.Bytes, BYTES_EXPECTED);
+        }
+
+        [Fact]
+        public void FromBytes_ShouldWork() {
+            byte[] BYTES = new byte[16];
+            
+            var principal = Principal.FromBytes(BYTES);
+
+            Assert.Equal(principal.Bytes, BYTES);
+        }
+
+        [Fact]
+        public void FromText_ShouldWork() {
+            var ANONYMOUS_TEXT = new string("2vxsx-fae");
+            byte[] BYTES_EXPECTED = { 4 };
+            
+            var principal = Principal.FromText(ANONYMOUS_TEXT);
+
+            Assert.Equal(principal.Bytes, BYTES_EXPECTED);
+        }
+
+        [Fact]
+        public void ToText_ShouldWork() {
+            byte[] ANONYMOUS_BYTES = { 4 };
+            var TEXT_EXPECTED = new string("2vxsx-fae");
+            
+            var principal = Principal.FromBytes(ANONYMOUS_BYTES);
+
+            var p = principal.ToString();
+    }
     }
 }
